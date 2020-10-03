2be5727fbabeff7f36f705a8078cc79d880ff99d
Dot <madnauk@gmail.com>
2020-10-03 10:00:03 +0300

    enabled formatting with black

--- main.py
+++ main.py
@@ -8,31 +8,35 @@ from ahk.window import Window
 import sample_items
 import logging
 import requests
+
 logging.basicConfig(level=logging.INFO)
 
 ahk = AHK()
 clipboard = pyperclip.paste()
 keyboard = Controller()
-script = f'ToolTip, {clipboard}, 500, 500'
+script = f"ToolTip, {clipboard}, 500, 500"
 hotkey = Hotkey(ahk, "^p", script)
 persistent = "#Persistent\n"
 sleepscr = "Sleep 1000"
 
+
 def quit_func():
     """ Function for properly closing ahk hotkey listener and quitting this script """
-    print('Executed quit_func')
+    print("Executed quit_func")
     hotkey.stop()
     quit()
+
 
 def display_item_in_tooltip(item):
     # https://www.autohotkey.com/docs/Tutorial.htm#s3
     # TODO: read up on {Raw} tag, might not need to format item text with it
     # TODO: reading ahk documentation first before writing this much MIGHT have been a good idea
-    ahk.run_script(f'ToolTip, \n(\n{item}\n)\n{sleepscr}')
+    ahk.run_script(f"ToolTip, \n(\n{item}\n)\n{sleepscr}")
+
 
 def format_raw_item(item):
     """ Cleans up raw clipboard item text for usage in ahk script """
-    strings_to_remove = ["\r", "--------\n", "\"", "'"]
+    strings_to_remove = ["\r", "--------\n", '"', "'"]
     result = item.replace("%", "perc")
     for string in strings_to_remove:
         result = result.replace(string, "")
@@ -40,12 +44,14 @@ def format_raw_item(item):
     result = "`n".join(result)  # join list elements with ahk's newline
     return result
 
+
 def clipboard_to_tooltip():
     """ Gets item's text from clipboard, formats it, shows a tooltip with it """
-    print('Caught ctrl-c')
+    print("Caught ctrl-c")
     # TODO: try removing just the 'c' key to see if i can make chaining tooltips possible with ctrl pressed down
     pressed_vks.clear()  # clearing pressed keys set to prevent weirdness, "There must be a better way!" (c)
-    sleep(0.01)  # i think it prevents what's called a race condition? else old clipboard data gets copied
+    # i think sleep prevents a race condition? else old clipboard data gets copied
+    sleep(0.01)
     win = ahk.active_window.process
     print(win)
     if "PathOfExile" in win:
@@ -53,8 +59,9 @@ def clipboard_to_tooltip():
         item = format_raw_item(clipboard)  # formats it
         # raw clipboard to tooltip function, but doesn't require formatting to work
         # ahk.run_script(f'ToolTip, {"".join(["%", "clipboard%"])}{sleepscr}')
-        display_item_in_tooltip(item)  # alternative, needs more work cleaning out illegal ahk chars
+        display_item_in_tooltip(item)
     print("---end---")
+
 
 def to_hideout():
     pressed_vks.clear()  # clearing pressed keys set to prevent weirdness, "There must be a better way!" (c)
@@ -67,6 +74,7 @@ def to_hideout():
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
         sleep(1)  # lord forgive me for this temporary solution while i'm learning stuff
+
 
 def pricecheck():
     pressed_vks.clear()  # clearing pressed keys set to prevent weirdness, "There must be a better way!" (c)
@@ -85,14 +93,19 @@ def pricecheck():
     # win = ahk.active_window.process
     # print(win)
     # if "PathOfExile" in win:
-    r = requests.get("https://poe.ninja/api/data/currencyoverview?league=Heist&type=Currency&language=en")
+    r = requests.get(
+        "https://poe.ninja/api/data/currencyoverview?league=Heist&type=Currency&language=en"
+    )
     r = r.json()
     # print(r.content)
     for line in r["lines"]:
         if item_info[1] == line["currencyTypeName"]:
             print("Hit", item_info[1])
-            print(f'{stack_size} {format(stack_size / float(line["pay"]["value"]), ".2f")}c')
+            print(
+                f'{stack_size} {format(stack_size / float(line["pay"]["value"]), ".2f")}c'
+            )
             break
+
 
 # Create a mapping of keys to function (use frozenset as sets/lists are not hashable - so they can't be used as keys)
 # Note the missing `()` after quit_func and clipboard_to_tooltip as want to pass the function, not the return value of the function
@@ -106,25 +119,29 @@ combination_to_function = {
 # The currently pressed keys (initially empty)
 pressed_vks = set()
 
+
 def get_vk(key):
     """
     Get the virtual key code from a key.
     These are used so case/shift modifications are ignored.
     """
-    return key.vk if hasattr(key, 'vk') else key.value.vk
+    return key.vk if hasattr(key, "vk") else key.value.vk
+
 
 def is_combination_pressed(combination):
     """ Check if a combination is satisfied using the keys pressed in pressed_vks """
     return all([get_vk(key) in pressed_vks for key in combination])
+
 
 def on_press(key):
     """ When a key is pressed """
     vk = get_vk(key)  # Get the key's vk
     pressed_vks.add(vk)  # Add it to the set of currently pressed keys
 
-    for combination in combination_to_function:  # Loop through each combination
-        if is_combination_pressed(combination):  # Check if all keys in the combination are pressed
-            combination_to_function[combination]()  # If so, execute the function
+    for combination in combination_to_function:
+        if is_combination_pressed(combination):
+            combination_to_function[combination]()
+
 
 def on_release(key):
     """ When a key is released """
@@ -132,6 +149,7 @@ def on_release(key):
     if vk in pressed_vks:
         pressed_vks.remove(vk)  # Remove it from the set of currently pressed keys
     pass
+
 
 with Listener(on_press=on_press, on_release=on_release) as listener:
     hotkey.start()
